df1b39911b2758e1416f7cbdd1727c50bbb3c82c
julivansoares <julivan.soares@outlook.com>
2023-08-05 20:33:08 -0300

    atualização do projeto

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,10 +1,21 @@
 import './Formulario.css'
 
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 
 
 
 function Formulario(){
+    const times =[
+        '',
+        'programação',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
     return(
         <section className='formulario'>
             <form>
@@ -12,6 +23,7 @@ function Formulario(){
                 <CampoTexto label='Nome' placeholder='Digite seu nome'/>
                 <CampoTexto label='Cargo' placeholder='Digite seu cargo'/>
                 <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem'/>
+                <ListaSuspensa label="Time" itens={times} />
             </form>
         </section>
 
